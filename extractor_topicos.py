1d879bba89210444d2d588f10cd4f671c7c98629
juanpablo-munoz <juan.munozm.13@sansano.usm.cl>
2017-08-23 03:26:03 -0300

    Crea CSV con separador ';'

--- extractor_topicos.py
+++ extractor_topicos.py
@@ -6,15 +6,15 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import NMF, LatentDirichletAllocation
 from sklearn.datasets import fetch_20newsgroups
 
-n_features = 1000 # <--- tamano del vocabulario a construir. Se descartan del vocabulario aquellas palabras con bajo Tf-Idf (aquellas que estadisticamente aportan menos significado). Probar varios valores para ver que tan bien resulta la extraccion de topicos
-n_components = 10 # <--- Cantidad de Tópicos a definir por los algoritmos. Probar variar este valor.
+n_features = 50 # <--- tamano del vocabulario a construir. Se descartan del vocabulario aquellas palabras con bajo Tf-Idf (aquellas que estadisticamente aportan menos significado). Probar varios valores para ver que tan bien resulta la extraccion de topicos
+n_components = 7 # <--- Cantidad de Tópicos a definir por los algoritmos. Probar variar este valor.
 n_top_words = 20 # <--- Cantidad de palabras pertenecientes a cada tópico definido
 
 
 def print_top_words(model, feature_names, n_top_words):
     for topic_idx, topic in enumerate(model.components_):
         message = "Topic #%d: " % topic_idx
-        message += " ".join([feature_names[i]
+        message += " ".join([feature_names[i]+'('+str(round(topic[i],3))+')'
                              for i in topic.argsort()[:-n_top_words - 1:-1]])
         print(message)
     print()
@@ -22,10 +22,21 @@ def print_top_words(model, feature_names, n_top_words):
 print("Loading dataset...")
 t0 = time()
 
+def topicos_tweets(documentos, feature_names, modelo):
+    resultado = []
+    for doc in documentos:
+        puntajes_por_topico = np.array([0]*len(modelo.components_))
+        for idx_topico, topico in enumerate(modelo.components_):
+            for palabra in doc.split():
+                if palabra in feature_names:
+                    puntajes_por_topico[idx_topico] += topico[feature_names.index(palabra)]
+        indice_topico = puntajes_por_topico.argsort()[-1]
+        indice_nombre_topico = modelo.components_[indice_topico].argsort()[-1]
+        resultado.append(feature_names[indice_nombre_topico])
+    return resultado
 
-dataset = pd.read_csv('tweets_nyctsubway.csv',
-                        encoding ='latin1',
-                        names=['texto']).texto
+
+dataset = pd.read_csv('tweets_nyctsubway.csv',encoding ='latin1').texto
 
 data_samples = np.unique(dataset.astype(str))
 n_samples = len(data_samples)
@@ -90,3 +101,9 @@ print("done in %0.3fs." % (time() - t0))
 print("\nTopics in LDA model:")
 tf_feature_names = tf_vectorizer.get_feature_names()
 print_top_words(lda, tf_feature_names, n_top_words)
+
+# Usar modelo 'lda' para asignar un topico a cada tweet en el archivo csv
+write_df = pd.Series(topicos_tweets(dataset.astype(str), tf_feature_names, lda))
+
+with open('tweets_nyctsubway.csv', 'a', encoding='utf8') as f:
+    write_df.to_csv(f, columns=['topico'], index=False, header=False)
